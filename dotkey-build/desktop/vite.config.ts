aea411a4c2909bef0db3ee487baf8ed55793b312
cantor-1 <cantorandludio@outlook.com>
2026-06-08 13:52:55 +0800

    Update vite.config.ts

--- dotkey-build/desktop/vite.config.ts
+++ dotkey-build/desktop/vite.config.ts
@@ -4,10 +4,19 @@ export default defineConfig({
   clearScreen: false,
   server: { port: 1420, strictPort: true },
   envPrefix: ['VITE_', 'TAURI_'],
+  optimizeDeps: {
+    include: ['libsodium-wrappers'],
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  },
   build: {
     target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     sourcemap: !!process.env.TAURI_DEBUG,
-    outDir: 'dist'
+    outDir: 'dist',
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
   }
 })
